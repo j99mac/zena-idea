c6e470288b4d5884062935453848ab998eb470e5
j99mac <j99mac@gmail.com>
2014-02-02 09:51:57 -0500

    update

--- contact-us-send.php
+++ contact-us-send.php
@@ -1,8 +1,8 @@
 <?php
-if (isset($_POST['submit'])) {
-$msg = 'Name: ' .$_POST['name'] ."\n"
-	.'Email: ' .$_POST['email'] ."\n"
-	.'Comment: ' ."\n" .$_POST['comment'];
+if (isset($_GET['submit'])) {
+$msg = 'Name: ' .$_GET['name'] ."\n"
+	.'Email: ' .$_GET['email'] ."\n"
+	.'Comment: ' ."\n" .$_GET['comment'];
 	mail("j99mac@gmail.com', Sample Comments', $msg);
 	header('location: contact-us-thank-you.html');	
  	echo "Thank you for sending us feedback";
