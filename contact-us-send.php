058793ede49a590da67daf87931204dcff948617
j99mac <j99mac@gmail.com>
2014-02-02 10:07:36 -0500

    update

--- contact-us-send.php
+++ contact-us-send.php
@@ -1,11 +1,5 @@
-<html>
-<body>
-
-Welcome <?php echo $_GET["name"]; ?><br>
-Your email address is: <?php echo $_GET["email"]; ?>
-Subject: <?php echo $_GET["subject"]; ?>
-Message: <?php echo $_GET["message"]; ?>
-
-
-</body>
-</html>
+<? php
+echo $_GET["name"] .<br />";
+echo $_GET["email] .<br />";
+echo $_GET["message"];
+?>
