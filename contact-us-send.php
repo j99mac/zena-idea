6e75a8c779e63fe6f4aa0cd9e59cd331bafeb57e
j99mac <j99mac@gmail.com>
2014-02-02 09:57:11 -0500

    update

--- contact-us-send.php
+++ contact-us-send.php
@@ -1,15 +1,11 @@
-<?php
-if (isset($_GET['submit'])) {
-$msg = 'Name: ' .$_GET['name'] ."\n"
-	.'Email: ' .$_GET['email'] ."\n"
-	.'Comment: ' ."\n" .$_GET['comment'];
-	mail("j99mac@gmail.com', Sample Comments', $msg);
-	header('location: contact-us-thank-you.html');	
- 	echo "Thank you for sending us feedback";
-  	}
-	else
-  	{
-  	echo "Please enter your email address to send feedback";
-  	}
-	exit(0);
-?>
+<html>
+<body>
+
+Welcome <?php echo $_GET["name"]; ?><br>
+Your email address is: <?php echo $_GET["email"]; ?>
+Subject: <?php echo $_GET["subject"]; ?>
+Message: <?php echo $_GET["message"]; ?>
+
+
+</body>
+</html>
