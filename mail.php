827e562b5dddaa2f566a6d793b9e4e0ecef77c2e
j99mac <j99mac@users.noreply.github.com>
2014-02-03 15:59:48 -0500

    Update mail.php

--- mail.php
+++ mail.php
@@ -6,11 +6,11 @@ $message = $_GET['message'];
 
 $todayis = date("l, F j, Y, g:i a") ;
 $subject = "A message sent on ".$todayis." from ".$name." via the playatics website";
-$message = " Message: $comment \r \n From: $name  \r \n Reply to: $email";
+$message = " Message: $message \r \n From: $name  \r \n Reply to: $email";
 $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
-$headers .= 'From: Domain Name j99mac@yahoo.com' . "\r\n";   
+$headers .= 'From: Domain Name j99mac@gmail.com' . "\r\n";   
 
-mail("j99mac@yahoo.com", $subject, $message, $headers);
+mail("j99mac@gmail.com", $subject, $message, $headers);
 
 
 echo "<br/> Thanks for your message: <b>$message</b>";
