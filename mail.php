dd74ce78c77860f0c17da98958bcb11bc84acb44
j99mac <j99mac@users.noreply.github.com>
2014-02-03 15:50:43 -0500

    Create mail.php

---
+++ mail.php
@@ -0,0 +1,18 @@
+<?php
+
+$name = $_GET['name'];
+$email = $_GET['email'];
+$message = $_GET['message'];
+
+$todayis = date("l, F j, Y, g:i a") ;
+$subject = "A message sent on ".$todayis." from ".$name." via the playatics website";
+$message = " Message: $comment \r \n From: $name  \r \n Reply to: $email";
+$headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+$headers .= 'From: Domain Name contact@domain.com' . "\r\n";   
+
+mail("somemail@domain.com", $subject, $message, $headers);
+
+
+echo "<br/> Thanks for your message: <b>$message</b>";
+
+?>
