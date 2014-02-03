e9b8db16ae10746b8ec114bf8f7c72bf952dfe85
j99mac <j99mac@users.noreply.github.com>
2014-02-03 16:03:43 -0500

    Update mail.php

--- mail.php
+++ mail.php
@@ -4,7 +4,7 @@ $name = $_GET['name'];
 $email = $_GET['email'];
 $message = $_GET['message'];
 
-mail("j99mac@gmail.com", $subject, $message, $headers);
+mail("j99mac@gmail.com", $name, $email, $message);
 
 
 echo "<br/> Thanks for your message: ;
