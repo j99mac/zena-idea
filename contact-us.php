c57058294f433cc3bc72fe2b3863bb6504c7f637
j99mac <j99mac@users.noreply.github.com>
2014-02-04 13:57:34 -0500

    Update contact-us.php

--- contact-us.php
+++ contact-us.php
@@ -3,7 +3,7 @@ if (isset($_POST['submit'])) {
 $msg = 'Name: ' .$_POST['name'] ."\n"
 	.'Email: ' .$_POST['email'] ."\n"
 	.'Comment: ' ."\n" .$_POST['comment'];
-	mail('example@example.com', Sample Comments', $msg);
+	mail('j99mac@yahoo.com', Sample Comments', $msg);
 	header('location: contact-us-thank-you.html');
 
 ) else (
